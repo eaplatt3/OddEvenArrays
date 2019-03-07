fcfd4d6f04b639a614e355f72321972500de92e3
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-03-07 14:40:22 -0500

    Initial commit

---
+++ OddEvenArrays.pl
@@ -0,0 +1,23 @@
+####################################
+# Earl Platt III
+# 3/7/2019
+# BCS 316 - Perl Programming
+# Odd & Even Array
+# Input - 1 - 10 Static Numbers
+# Output - Two arrays one with odd 
+#          Numbers one with Even 
+####################################
+
+use strict;
+use warnings;
+
+#Variables
+my @nums = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+my @oddNums;
+my @evenNums;
+
+#Array Splicing 
+@oddNums = @nums[0,2,4,6,8,10];
+@evenNums = @nums[1,3,5,7,9];
+
+print @oddNums;
